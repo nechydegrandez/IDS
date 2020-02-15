2399e8ea886eb8c60bce7e66fc9b780741886cdc
nechydegrandez <44045461+nechydegrandez@users.noreply.github.com>
2020-02-15 03:06:42 -0600

    menu

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="css/agregarpedido.css" rel="stylesheet">
+</head>
+<body>
+  <!--- aqui incluyo el menu-->
+    <?php
+    include ("menu.html") ;
+      ?> 
+    
+  
+<h1>Agregar Nuevo Pedido</h1>
+
+
+<form  class="inf" action="">
+<pre>Empresa: <select name="empresa" id="empresa">
+                           Sucursal: <select name="empresa" id="empresa"></select></pre>
+
+</form>
+<div class="inf">
+<pre>Fecha registro del pedido <select name="empresa" id="empresa"></select> <select name="empresa" id="empresa"></select> <select name="empresa" id="empresa"></select>  </pre>
+
+
+
+</div>
+
+
+
+
+      <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+    
+    <script src="js/menu.js"></script>
+</body>
+</html>
