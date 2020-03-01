0450110526a6429725837e40a0592401d25f273d
LPSoldier19 <rafael.bautista1@hotmail.es>
2020-03-01 10:00:20 -0600

    Mas Commits

--- nueva_sucursal.php
+++ nueva_sucursal.php
@@ -3,45 +3,71 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/menu.css" rel="stylesheet">
+    <title>Devoluciones</title>
+    <link href="css/facturas.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-    <ul class="accordion-menu">
-        <li>
-          <div class="dropdownlink"><i class="fa fa-road" aria-hidden="true"></i> Menu Principal
-            <i class="fa fa-chevron-down" aria-hidden="true"></i>
-          </div>
-         
-        </li>
-        <li>
-          <div class="dropdownlink"><i class="fa fa-paper-plane" aria-hidden="true"></i> Pedidos
-            <i class="fa fa-chevron-down" aria-hidden="true"></i>
-          </div>
-          <ul class="submenuItems">
-            <li><a href="#">Nuevo Pedido</a></li>
-            <li><a href="#">Ver Pedidos</a></li>
-          </ul>
-        </li>
-        <li>
-          <div class="dropdownlink"><i class="fa fa-quote-left" aria-hidden="true"></i> Facturas
-            <i class="fa fa-chevron-down" aria-hidden="true"></i>
-          </div>
-          <ul class="submenuItems">
-            
-          </ul>
-        </li>
-        <li>
-          <div class="dropdownlink"><i class="fa fa-motorcycle" aria-hidden="true"></i> Inventario
-            <i class="fa fa-chevron-down" aria-hidden="true"></i>
-          </div>
-          <ul class="submenuItems">
-            <li><a href="#">Registrar producto</a></li>
-            <li><a href="#">Registrar Insumo</a></li>
-            <li><a href="#">Inventario producto</a></li>
-            <li><a href="#">Inventario Insumo</a></li>
-          </ul>
-        </li>
-      </ul>
+  <!--- aqui incluyo el menu-->
+    <?php
+    include ("menu.html") ;
+      ?> 
+    
   
+<h1 >Facturas</h1>
+
+
+<form  class="inf" action="">
+  <div style="font-size: 20px" >Empresa      
+    <select name="empresa" id="empresa" style="margin-left: 15px; padding-left:100px;">
+    Sucursal: <select name="empresa" id="empresa"></select>
+  </div>
+</form>
+
+<div class="inf align-items-center" style="font-size: 15px;">
+  <div>
+  <table class="table table-striped">
+  <thead>
+    <tr>
+      <th class="text-center">Codigo del Pedido</th>
+      <th class="text-center">Sucursal</th>
+      <th class="text-center">Fecha</th>
+      <th class="text-center">Total Factura</th>
+      <th class="text-center">       </th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th class="text-center">1</th>
+      <td class="text-center">La Colonia #12</td>
+      <td class="text-center">27/02/202</td>
+      <td class="text-center">200.00</td>
+      <td class="text-center"><a href="#" download="">Imprimir</a></td> <!-- Este boton descargara la informacion del pedido, para despues imprimirlo -->
+    </tr>
+  </tbody>
+  </table>
+  <nav aria-label="...">
+  <ul class="pagination pagination-sm">
+    <li class="page-item active" aria-current="page">
+      <span class="page-link">
+        1
+        <span class="sr-only">(current)</span>
+      </span>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+  </ul>
+</nav>
+  </div>
+
+
+</div>
+
+
+
+
+      <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+    
+    <script src="js/menu.js"></script>
 </body>
 </html>
