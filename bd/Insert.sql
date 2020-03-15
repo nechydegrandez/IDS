7908fa75e7ae66a351370f5af98d77601b1f92e5
Maria Jose Gutierrez Barahona <43561043+Majoguti@users.noreply.github.com>
2020-03-14 20:19:53 -0600

    Add files via upload
    INSERT CORRECTOS PRROS:V

---
+++ bd/Insert.sql
@@ -0,0 +1,29 @@
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (1,'Paulo','Andre','Londra','Rubio',08001-1995-09876,'pauloandre_95@hotmail.com','Col.Kennedy','991934-45','m')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (2,'Mario','Antonio','Lopez','Barahona',08011-1997-06776,'Marioan_97@gmail.com','Col.Miraflores','998904-95','m')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (2,'Mario','Antonio','Lopez','Barahona',08011-1997-06776,'Marioan_97@gmail.com','Col.Miraflores','997977-95','m')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (3,'Samanta','Maria','Garcia','Martinez',08001-1997-39896,'Saman_97@gmail.com','Col.Navio','961964-85','f')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (4,'Fernando','Daniel','Garcia','Mendez',0801-1994-280394,'Fernda_94@gmail.com','Col.Llanos','991934-45','m')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (5,'Nolan','Said','Pavon','Martinez',08001-1993-76543,'Nsaid_93@gmail.com','Col.Jose','901934-05','m')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (6,'German','David','Garcia','Guevara',08001-1998-04386,'German_98@gmail.com','Col.Aleman','971984-55','m')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (7,'Sabrina','Marie','Lopez','Barahona',08001-1993-08421,'Marie_93@gmail.com','Col.Jacaleapa','891984-15','f')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (8,'Maria','Jose','	Hernandez','Cardona',08001-1995-08763,'mjHer_95@gmail.com','Col.SanMiguel','871934-25','f')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (9,'Carlos','Roberto','Motiño','Franco',08011-1995-09876,'Carlosr_95@gmail.com','Col.Pedregal','819934-35','m')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (10,'Francis','Ruby','Godines','Ramos',08001-1994-08790,'Rubygo_94@gmail.com','Col.Vega','959340-15','f')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (11,'Oscar','Ovany','Calix','Reyes',08001-1992-04833,'Oscaro_92@gmail.com','Col.America','941734-05','m')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (12,'Bryan','Leonardo','Rodriguez','Alvarez',08001-1993-08653,'Leoro_93@gmail.com','Col.San Angel','973934-75','m')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (13,'Luis','Fernando','Estrada','Nuñez',08001-1991-08721,'luisf_91@gmail.com','Col.San Francisco','987934-05','m')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (14,'Heidy','Mabel','Gomez','Erazo',08001-1994-05098,'Hmabel_94@gmail.com','Col.Chimbo','964934-89','f')
+INSERT INTO persona(idPersona,PNombre,SNombre,PApellido,SApellido,n_identidad,correo,direccion,telefono,genero) VALUES (15,'Angel','David','Vargas','Guzman',08001-1993-09876,'Angeld_93@gmail.com','Col.Suyapa','990904-45','m')
+
+
+INSERT INTO Tipo_usuario(idTipoUsuario,tipo_usuario )VALUES (1,'Administrador')
+INSERT INTO Tipo_usuario(idTipoUsuario,tipo_usuario )VALUES (2,'Empleado')
+
+
+INSERT INTO Usuario
+(id_usuario,contrasenia,TipoUsuario_idTipoUsuario,estado)VALUES('1''asd.456''2','a')
+INSERT INTO Usuario
+(id_usuario,contrasenia,TipoUsuario_idTipoUsuario,estado)VALUES('2''asd.456''1','a')
+INSERT INTO Usuario
+(id_usuario,contraseniaTipoUsuario_idTipoUsuario,estado)VALUES('3''asd.456''2','a')
+(id_usuario,contrasenia,TipoUsuario_idTipoUsuario,estado)VALUES('4''asd.456''2','i')
