f83d0f29331c965ad48fa48d05199e271550c401
nechydegrandez <44045461+nechydegrandez@users.noreply.github.com>
2020-03-14 19:08:42 -0600

    CONEXION

---
+++ class/conexion.php
@@ -0,0 +1,12 @@
+<?php
+    $serverName = "localhost";
+    //AGREGAR EL NOMBRE DE LA BASE DE DATOS CUALQUIERA Y LA CONTRASEÑA DE USUARIO , YO LA QUITE POR QUE ES ALGO PERSONAL JAJAJ
+    $infoConnection = array("Database"=>"BD","UID"=>"user","PWD"=>"123","CharacterSet"=>"UTF-8");
+    try {
+        $conexion = sqlsrv_connect($serverName,$infoConnection);
+
+    } catch (Execption $e) {
+        die('Connected Failed: ' . $e->getMessage());
+    }
+
+?>
