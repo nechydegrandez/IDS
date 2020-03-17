f3f603bd062b06be02fef86ee94fdc4eb3f5c261
nechydegrandez <44045461+nechydegrandez@users.noreply.github.com>
2020-03-16 21:01:18 -0600

    bd

--- class/conexion.php
+++ class/conexion.php
@@ -1,7 +1,7 @@
 <?php
     $serverName = "localhost";
     //AGREGAR EL NOMBRE DE LA BASE DE DATOS CUALQUIERA Y LA CONTRASEÑA DE USUARIO , YO LA QUITE POR QUE ES ALGO PERSONAL JAJAJ
-    $infoConnection = array("Database"=>"BD","UID"=>"user","PWD"=>"123","CharacterSet"=>"UTF-8");
+    $infoConnection = array("Database"=>"Espumillas","UID"=>"user","PWD"=>"123","CharacterSet"=>"UTF-8");
     try {
         $conexion = sqlsrv_connect($serverName,$infoConnection);
 
