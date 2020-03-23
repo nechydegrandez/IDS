909eeeed2036bc9cc47063d3e4c58774827fc233
nechydegrandez <44045461+nechydegrandez@users.noreply.github.com>
2020-03-23 03:47:02 -0600

    conexion

--- class/conexion.php
+++ class/conexion.php
@@ -1,12 +1,97 @@
 <?php
-    $serverName = "localhost";
-    //AGREGAR EL NOMBRE DE LA BASE DE DATOS CUALQUIERA Y LA CONTRASEÑA DE USUARIO , YO LA QUITE POR QUE ES ALGO PERSONAL JAJAJ
-    $infoConnection = array("Database"=>"Espumillas","UID"=>"user","PWD"=>"123","CharacterSet"=>"UTF-8");
-    try {
-        $conexion = sqlsrv_connect($serverName,$infoConnection);
+	class Conexion{
+		
+		private $host;
+		private $database;
+		private $usuario;
+		private $pass;
+		private $link;
+		private $puerto;
+		private $esConectado=false;
+		public function __construct(
+			$host =  "localhost",
+			$database =  "espumillas", //colocan el nombre de la base de datos 
+			$usuario =  "root",//usuario
+			$pass =  "",
+			$port = 3308,
+			$link = null
+		){
+			$this->host = $host;
+			$this->database = $database;
+			$this->usuario = $usuario;
+			$this->pass = $pass;
+			$this->port = $port;
+			$this->establecerConexion();
+		}
+		public function establecerConexion () {
+			// MySQLi
+			$this->link = mysqli_connect(
+				$this->host,
+				$this->usuario,
+				$this->pass,
+				$this->database,
+				$this->puerto
+			);
+			if (!$this->link){
+				$this->esConectado = false;
+				echo "No se pudo conectar con mysql";
+				exit;
+			} else {
+				mysqli_set_charset($this->link,"utf8");
+                $this->esConectado = true;
+                echo "conectato amor con mysql";
 
-    } catch (Execption $e) {
-        die('Connected Failed: ' . $e->getMessage());
-    }
-
+			}
+		}
+		public function antiInyeccion($texto){
+			return mysqli_real_escape_string($this->link, $texto);
+		}
+		public function getResultadoQuery($sql, $valores){
+			if ($this->esConectado) {
+				for ($i=0; $i < count($valores) ; $i++){
+					$valores[$i] = $this->antiInyeccion($valores[$i]);
+				}
+				$consulta = vsprintf($sql, $valores);
+				$resultado = mysqli_query($this->link, $consulta);
+				return $resultado;
+			}else{
+				return null;
+			}
+		}
+		public function getFila($resultado){
+			return mysqli_fetch_array($resultado, MYSQLI_ASSOC);
+		}
+		// Retorna los registros asociados a una consulta SQL
+		// en la base de datos
+		public function query($sql, $valores = []){
+			if($this->esConectado){
+				$registros = [];
+				$resultado = $this->getResultadoQuery($sql, $valores);
+				if ($resultado != null) {
+					while ($fila = $this->getFila($resultado)) {
+						$registros[] = $fila;
+					}
+				}
+				$this->liberarResultado($resultado);
+				return $registros;
+			}else{
+				return null;
+			}
+		}
+		public function liberarResultado ($resultado) {
+			mysqli_free_result($resultado);
+		}
+		public function ejecutarConsulta($sql){
+			return mysqli_query ($this->link, $sql);
+		}
+		public function obtenerFila($resultado){
+			return mysqli_fetch_array ($resultado);
+		}
+		public function cantidadRegistros($resultado){
+			return mysqli_num_rows($resultado);
+		}
+		public function cerrar(){
+			mysqli_close($this->link);
+		}
+	}
 ?>
