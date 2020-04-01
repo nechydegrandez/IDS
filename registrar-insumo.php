1e53a274e380f0a4778a81755a7e4a6cb76bcf93
LPSoldier19 <rafael.bautista1@hotmail.es>
2020-03-31 18:00:20 -0600

    Commit Final de las Paginas

--- registrar-insumo.php
+++ registrar-insumo.php
@@ -15,9 +15,9 @@
       ?> 
 
 
-    <h1 style="color: white;">Registrar Nuevo Producto</h1>
+    <h1 style="color: white; padding-left:525px;">Registrar Insumo</h1>
 
-    <div class="inf card align-items-center pr-5" style="font-size: 15px;"> <!--contenedor de items debajo del titulo-->
+    <div class="inf card align-items-center pr-5 pt-5 pb-5" style="font-size: 15px;"> <!--contenedor de items debajo del titulo-->
     <div class="row">
       <p>Nombre del Producto:</p>
       <textarea name="body" id="edit-body" cols="50" rows="0.5" class="upd-01" aria-hidden="true" style="margin-left: 80px;">
@@ -29,16 +29,13 @@
     </textarea>
   </div>
   <div class="row" style="margin-top: 15px;">
-    <p>Precio de Venta:</p>
+    <p>Precio de Compra:</p>
     <textarea name="body" id="edit-body" cols="50" rows="0.5" class="upd-01" aria-hidden="true" style="margin-left: 125px;">
     </textarea>
   </div>
   <div class="row" style="margin-top: 15px;">
-   <p>Categoria:</p>
    <div class="dropdown" style="margin-left: 55px;">
-    <a class="btn btn-presionar dropdown-toggle btn-h btn-grupos" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      -----------
-    </a>
+
 </div>
 
   </div>
