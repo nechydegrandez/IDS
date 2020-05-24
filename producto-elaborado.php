8c2dab4db6a3ecb384a849cc42bb0443bbe5f62d
nechydegrandez <44045461+nechydegrandez@users.noreply.github.com>
2020-05-23 23:48:10 -0600

    pedidos

---
+++ producto-elaborado.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Inicio</title>
+    <link rel="icon" type="image/png" href="img/favicon.png">
+    <link href="css/inventarioproductoelaborado.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+</head>
+<body>
+  <!--- aqui incluyo el menu-->
+    <?php
+    include ("menu.html") ;
+      ?> 
+
+ <form action="">
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ </form>   
+
+
+ 
+
+
+
+      <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+    
+    <script src="js/menu.js"></script>
+    <script src="js/controlador.js"></script>
+</body>
+</html>
